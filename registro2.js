3e75220e368e08b55fe02d83935504adccfcc695
dianariverabarrero <50120928+dianariverabarrero@users.noreply.github.com>
2021-10-02 21:40:52 -0500

    Create registro2.js
    creación registro 2

---
+++ registro2.js
@@ -0,0 +1,12 @@
+function ordenarArreglo_Apellido(arreglo){
+
+
+
+}
+
+function retornarRegistroMedio(args){
+
+}
+
+module.exports.ordenarArreglo_Apellido = ordenarArreglo_Apellido();
+module.exports.ordenarArreglo_Apellido = retornarRegistroMedio();
