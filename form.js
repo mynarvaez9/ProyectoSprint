488728ab7b5ee7cdab61d246d2ab9a9f26c70036
mynarvaez9 <87199739+mynarvaez9@users.noreply.github.com>
2021-10-05 17:53:37 -0500

    se inluye archivo prueba js y modificacion de script

--- form.js
+++ form.js
@@ -42,35 +42,6 @@ function validar_contrasena(string){
     }
 }
 
-var registros = [];
-
-function agregarRegistro (){
-    
-    var nombreInput = document.getElementById("CampoNombre");
-    var apellidoInput = document.getElementById("CampoApellido");
-    var campoUsuarioInput = document.getElementById("campoUsuario");
-    var contrasenaInput = document.getElementById("campoContrasena");
-    var confirmacionContrasenaInput = document.getElementById("campoConfirmacionContrasena");
-    var correoInput = document.getElementById("campoEmail");
-    var confirmacionCorreoInput = document.getElementById("CampoConfirmacionEmail");
-    var telefonoInput = document.getElementById("CampoTelefono");
-    var direccionInput = document.getElementById("CampoDireccion");
-
-    var nuevoRegistro = {
-        CampoNombre: nombreInput.value,
-        CampoApellido: apellidoInput.value,
-        campoUsuario: campoUsuarioInput.value,
-        campoContrasena: contrasenaInput.value,
-        campoConfirmacionContrasena:confirmacionContrasenaInput.value,
-        campoEmail: correoInput.value,
-        CampoConfirmacionEmail:confirmacionCorreoInput.value,
-        CampoTelefono: telefonoInput.value,
-        CampoDireccion: direccionInput.value,
-    };
-
-    registros.push(nuevoRegistro);
-    
-} 
 
 module.exports.validar_usuario = validar_usuario;
 module.exports.validar_contrasena = validar_contrasena;
